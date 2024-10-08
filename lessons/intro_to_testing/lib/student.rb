2b7ade68015375bba7b36bf882d83aa933fb161e
Alonso Jurado <alonso.jj12@gmail.com>
2024-10-08 17:15:49 -0600

    Intro to Testing

---
+++ lessons/intro_to_testing/lib/student.rb
@@ -0,0 +1,12 @@
+class Student
+    attr_reader :name, #method that gets the value of an attribute
+                :cookies 
+    def initialize(name)
+        @name = name # attribute
+        @cookies = []
+    end
+
+    def add_cookie(cookie)
+        @cookies << cookie
+    end
+end
